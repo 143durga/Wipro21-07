dbf1c2da63138a3a9b27747b2d9adda588aa9ef6
Durga <112899033+143durga@users.noreply.github.com>
2025-08-10 17:33:42 +0530

    Delete 26-07-2025 directory

--- 26-07-2025/src/com/wipro/Solid/PhonePay.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.Solid;
-
-public class PhonePay implements Payment {
-	
-	public void makePayment(double amount) {
-        System.out.println("Paid ₹" + amount + " using PhonePay");
-    }
-
-}
