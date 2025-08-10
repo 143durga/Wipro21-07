2ea3bed444f743885e1bd7a78244b3bd1cdf1988
Durga <112899033+143durga@users.noreply.github.com>
2025-08-10 17:11:19 +0530

    Delete 23-07-2025 directory

--- 23-07-2025/src/com/wipro/oop/PaymentMethod.java
+++
@@ -1,6 +0,0 @@
-package com.wipro.oop;
-
-public interface PaymentMethod {
-	public void pay(double amount);
-
-}
--- 23-07-2025/src/com/wipro/oop/phonePay.java
+++
@@ -1,7 +0,0 @@
-package com.wipro.oop;
-
-public class phonePay implements PaymentMethod {
-    public void pay(double amount) {
-        System.out.println("Paid using PhonePay" +" " + amount);
-    }
-}
