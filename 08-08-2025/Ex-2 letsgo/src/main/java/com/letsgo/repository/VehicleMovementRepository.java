ff4c591dbfa9125815789557371b5baefad7b95e
Durga <112899033+143durga@users.noreply.github.com>
2025-08-10 17:10:04 +0530

    Delete 08-08-2025 directory

--- 08-08-2025/Ex-2 letsgo/src/main/java/com/letsgo/repository/VehicleMovementRepository.java
+++
@@ -1,7 +0,0 @@
-package com.letsgo.repository;
-
-import com.letsgo.entity.VehicleMovement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleMovementRepository extends JpaRepository<VehicleMovement, Long> {
-}
